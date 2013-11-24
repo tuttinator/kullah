62b9b8be42598251abe0f152ed192395aeca91b0
Caleb Tutty <caleb@prettymint.co.nz>
2013-11-24 22:31:29 +1300

    Creating routes and dummy json data

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,37 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect(routes.Application.colours)
+  }
+
+
+  def colours = Action {
+    Ok(
+      Json.obj(
+        "users" -> Json.arr(
+          Json.obj(
+            "name" -> "Bob",
+            "age" -> 31,
+            "email" -> "bob@gmail.com"
+          ),
+          Json.obj(
+            "name" -> "Kiki",
+            "age" -> 25,
+            "email" -> JsNull
+          )
+        )
+      )
+    )
+  }
+
+  def colour(id: Long) = Action {
+    Ok(views.html.index("yo"))
   }
 
 }
+
