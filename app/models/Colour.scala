d0880c17191cc4e5b924bd1ca1caf2a1ab9b57c3
Caleb Tutty <caleb@prettymint.co.nz>
2013-11-26 20:48:02 +1300

    Model changes

--- app/models/Colour.scala
+++ app/models/Colour.scala
@@ -15,26 +15,23 @@ object Colour {
     get[String]("hex") map {
       case id~name~hex => Colour(id, name, hex)
     }
-    
   }
 
-  def all(): List[Colour] = DB.withConnection { implicit c =>
+  def all(): List[Colour] = DB.withConnection { implicit connection =>
     SQL("select * from colour").as(colour *)
   }
 
-  def findById(id: Long): Option[Colour] = {
-    DB.withConnection { implicit connection =>
-      SQL("select * from colour where id = {id}").on(
-        'id -> id
-      ).as(colour *)
-    }
+  def findById(id: Long) = DB.withConnection { implicit connection =>
+    SQL("select * from colour where id = {id}").on(
+      'id -> id
+    ).as(colour *)
   }
 
-  def findByName(name: String) {
+  def findByName(name: String) = DB.withConnection { implicit connection =>
     SQL("select * from colour where name = {name} limit 1").as(colour *)
   }
 
-  def findByHex(hex: String) {
+  def findByHex(hex: String) = DB.withConnection { implicit connection =>
     SQL("select * from colour where hex = {hex} limit 1").as(colour *)
   }
 
